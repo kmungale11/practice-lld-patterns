d6d460f01f761ec30e54dfac3aa43449ab1ffa17
kmungale11 <kaustubh.mungale@gmail.com>
2025-12-18 00:38:59 -0500

    Behavioral pattern - Strategy pattern

---
+++ patterns/behaviour/strategy/Solution.java
@@ -0,0 +1,48 @@
+package patterns.behaviour.strategy;
+
+
+interface BaseStrategy {
+
+    void connect();
+}
+
+class MySQLService implements BaseStrategy {
+
+    @Override
+    public void connect() {
+        System.out.println("Connected to MySQL");
+    }
+}
+
+class PostgresSQLService implements BaseStrategy {
+    @Override
+    public void connect() {
+        System.out.println("Connected to PostgresSQL");
+    }
+}
+
+
+class MainApplication {
+    BaseStrategy strategy;
+
+    public BaseStrategy getStrategy(String name) {
+        if(name.equals("mysql")) {
+            strategy = new MySQLService();
+        } else {
+            strategy = new PostgresSQLService();
+        }
+        return strategy;
+    }
+    public void connectToDB(String name) {
+        BaseStrategy baseStrategy = getStrategy(name);
+        baseStrategy.connect();
+    }
+}
+
+public class Solution {
+    public static void main(String[] args ){
+        MainApplication application = new MainApplication();
+        application.connectToDB("mysql");
+    }
+
+}
